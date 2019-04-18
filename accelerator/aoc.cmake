758b7cd77376f4e97a182fe5db6c4b4aa1373f3d
James Liu <james2006roger@ayoo.ca>
2019-04-18 18:02:51 -0400

    Modified the CMakeLists.txt and aoc.cmake

--- accelerator/aoc.cmake
+++ accelerator/aoc.cmake
@@ -1,104 +1,63 @@
 # Functions used to generate the hardware targets
 
-function(add_hw_emulation_target)
+function (add_aoc_target)
     set (options )
-    set (oneValueArgs TARGET_NAME HEADER_DIR RTL_DIR RTL_LIB)
+    set (oneValueArgs TARGET_NAME TARGET_TYPE HEADER_DIR RTL_DIR RTL_LIB)
     set (multiValueArgs  SOURCES_LIST PREPROCESSOR_DEFS_LIST)
 
-    cmake_parse_arguments(add_hw_emulation_target "${options}" "${oneValueArgs}" "${multiValueArgs}" "${ARGN}" )   
+    cmake_parse_arguments(add_aoc_target "${options}" "${oneValueArgs}" "${multiValueArgs}" "${ARGN}" )    
+
+    #Initialize the aoc compilation flags with the common elements for all compilation targets
+    list (APPEND occflags -v -report -fp-relaxed
+            ${add_aoc_target_PREPROCESSOR_DEFS_LIST}
+            -I ${add_aoc_target_HEADER_DIR}
+            -I $ENV{INTELFPGAOCLSDKROOT}/include/kernel_headers)       
     
-    list(REMOVE_DUPLICATES add_hw_emulation_target_SOURCES_LIST)
-    list(SORT add_hw_emulation_target_SOURCES_LIST)
-
-    if ("${add_hw_emulation_target_PREPROCESSOR_DEFS_LIST}" STREQUAL "")
-    else()
-        list(REMOVE_DUPLICATES add_hw_emulation_target_PREPROCESSOR_DEFS_LIST)
-    endif()
-
-    set (target_name_local "${add_hw_emulation_target_TARGET_NAME}_hw_emu")
-
-    list (APPEND occflags -v -report -march=emulator -emulator-channel-depth-model=strict -fp-relaxed
+    #Check whether a valid build type is selected
+    if ("${add_aoc_target_TARGET_TYPE}" STREQUAL "EMULATION")
+        message (STATUS "Generating the emulation AOC target")
+        set (target_name_local "${add_aoc_target_TARGET_NAME}_aoc_emulation")
+        list (APPEND occflags 
+                -march=emulator 
+                -emulator-channel-depth-model=strict
                 -DEMULATOR
-                ${add_hw_emulation_target_PREPROCESSOR_DEFS_LIST}
                 -o ${target_name_local}
-                -I ${add_hw_emulation_target_HEADER_DIR}
-                -I $ENV{INTELFPGAOCLSDKROOT}/include/kernel_headers)
-
-    if ("${add_hw_emulation_target_RTL_LIB}" STREQUAL "")
-    else()
-        list (APPEND occflags -I ${add_hw_emulation_target_RTL_DIR} -L ${add_hw_emulation_target_RTL_DIR}
-                -l ${add_hw_emulation_target_RTL_LIB})
-    endif()
-
-    list (APPEND occflags ${add_hw_emulation_target_SOURCES_LIST})
-    
-    add_custom_target(${target_name_local}
-        COMMAND aoc ${occflags}
-    )
-    
-endfunction()
-
-function(add_hw_report_target)
-
-    set (options )
-    set (oneValueArgs TARGET_NAME HEADER_DIR RTL_DIR RTL_LIB)
-    set (multiValueArgs  SOURCES_LIST PREPROCESSOR_DEFS_LIST)
-
-    cmake_parse_arguments(add_hw_report_target "${options}" "${oneValueArgs}" "${multiValueArgs}" "${ARGN}" )  
-
-    list(REMOVE_DUPLICATES add_hw_report_target_SOURCES_LIST)
-    list(SORT add_hw_report_target_SOURCES_LIST)
-
-    if ("${add_hw_report_target_PREPROCESSOR_DEFS_LIST}" STREQUAL "")
-    else()
-        list(REMOVE_DUPLICATES add_hw_report_target_PREPROCESSOR_DEFS_LIST)
-    endif()
-
-    set (target_name_local "${add_hw_report_target_TARGET_NAME}_hw_report")
-    
-    list (APPEND occflags -v -report -c -fp-relaxed
+             )
+    elseif("${add_aoc_target_TARGET_TYPE}" STREQUAL "RTL_ONLY")
+        message (STATUS "Generating the RTL_ONLY AOC target")
+        set (target_name_local "${add_aoc_target_TARGET_NAME}_aoc_rtl_only")
+        list (APPEND occflags 
+                -c
                 -o ${target_name_local}
-                ${add_hw_report_target_PREPROCESSOR_DEFS_LIST}
-                -I ${add_hw_report_target_HEADER_DIR}
-                -I $ENV{INTELFPGAOCLSDKROOT}/include/kernel_headers)
-
-    if ("${add_hw_report_target_RTL_LIB}" STREQUAL "")
-    else()
-        list (APPEND occflags -I ${add_hw_report_target_RTL_DIR} -L ${add_hw_report_target_RTL_DIR}
-                -l ${add_hw_report_target_RTL_LIB})
-    endif()
-
-    list (APPEND occflags ${add_hw_report_target_SOURCES_LIST})
-    
-    add_custom_target(${target_name_local}
-        COMMAND aoc ${occflags}
-    )
-    
-endfunction()
-
-function(add_hw_profile_target)
-
-    set (options )
-    set (oneValueArgs TARGET_NAME HEADER_DIR RTL_DIR RTL_LIB)
-    set (multiValueArgs  SOURCES_LIST PREPROCESSOR_DEFS_LIST)
-
-    cmake_parse_arguments(add_hw_profile_target "${options}" "${oneValueArgs}" "${multiValueArgs}" "${ARGN}" )  
-
-    list(REMOVE_DUPLICATES add_hw_profile_target_SOURCES_LIST)
-    list(SORT add_hw_profile_target_SOURCES_LIST)
-
-    if ("${add_hw_profile_target_PREPROCESSOR_DEFS_LIST}" STREQUAL "")
-    else()
-        list(REMOVE_DUPLICATES add_hw_profile_target_PREPROCESSOR_DEFS_LIST)
-    endif()
-
-    set (target_name_local "${add_hw_profile_target_TARGET_NAME}_hw_profile")
-    
-    list (APPEND occflags -v -report -fp-relaxed -profile -high-effort
+             )
+    elseif("${add_aoc_target_TARGET_TYPE}" STREQUAL "PROFILE_HW")
+        message (STATUS "Generating the PROFILE_HW AOC target")
+        set (target_name_local "${add_aoc_target_TARGET_NAME}_aoc_profile_hw")
+        list (APPEND occflags 
+                -profile
+                -high-effort
                 -o ${target_name_local}
-                ${add_hw_profile_target_PREPROCESSOR_DEFS_LIST}
-                -I ${add_hw_profile_target_HEADER_DIR}
-                -I $ENV{INTELFPGAOCLSDKROOT}/include/kernel_headers)
+             ) 
+    elseif("${add_aoc_target_TARGET_TYPE}" STREQUAL "NORMAL_HW")
+        message (STATUS "Generating the NORMAL_HW AOC target")
+        set (target_name_local "${add_aoc_target_TARGET_NAME}_aoc_normal_hw")
+        list (APPEND occflags
+                -o ${target_name_local}
+             )
+    elseif("${add_aoc_target_TARGET_TYPE}" STREQUAL "RELEASE_HW")
+        message (STATUS "Generating the RELEASE_HW AOC target")
+        set (target_name_local "${add_aoc_target_TARGET_NAME}_aoc_release_hw")
+        list (APPEND occflags 
+                -high-effort
+                -o ${target_name_local}
+             )
+    else()
+        message (FATAL_ERROR "Illegal TARGET_TYPE passed to the function add_aoc_target. Valid options are EMULATION, RTL_ONLY, PROFILE_HW, 
+            NORMAL_HW, and RELEASE_HW.")
+    endif()
+
+    list(REMOVE_DUPLICATES add_aoc_target_SOURCES_LIST)
+    list(SORT add_aoc_target_SOURCES_LIST)
 
     if ("${add_hw_emulation_target_RTL_LIB}" STREQUAL "")
     else()
@@ -106,45 +65,7 @@ function(add_hw_profile_target)
                 -l ${add_hw_profile_target_RTL_LIB})
     endif()
 
-    list (APPEND occflags ${add_hw_profile_target_SOURCES_LIST})
-    
-    add_custom_target(${target_name_local}
-        COMMAND aoc ${occflags}
-    )
-    
-endfunction()
-
-function(add_hw_release_target)
-
-    set (options )
-    set (oneValueArgs TARGET_NAME HEADER_DIR RTL_DIR RTL_LIB)
-    set (multiValueArgs  SOURCES_LIST PREPROCESSOR_DEFS_LIST)
-
-    cmake_parse_arguments(add_hw_release_target "${options}" "${oneValueArgs}" "${multiValueArgs}" "${ARGN}" )  
-
-    list(REMOVE_DUPLICATES add_hw_release_target_SOURCES_LIST)
-    list(SORT add_hw_release_target_SOURCES_LIST)
-
-    if ("${add_hw_release_target_PREPROCESSOR_DEFS_LIST}" STREQUAL "")
-    else()
-        list(REMOVE_DUPLICATES add_hw_release_target_PREPROCESSOR_DEFS_LIST)
-    endif()
-
-    set (target_name_local "${add_hw_release_target_TARGET_NAME}_hw_release")
-    
-    list (APPEND occflags -v -report -fp-relaxed -high-effort
-                -o ${target_name_local}
-                ${add_hw_release_target_PREPROCESSOR_DEFS_LIST}
-                -I ${add_hw_release_target_HEADER_DIR}
-                -I $ENV{INTELFPGAOCLSDKROOT}/include/kernel_headers)
-
-    if ("${add_hw_release_target_RTL_LIB}" STREQUAL "")
-    else()
-        list (APPEND occflags -I ${add_hw_release_target_HEADER_DIR} -L ${add_hw_release_target_RTL_DIR}
-                -l ${add_hw_release_target_RTL_LIB})
-    endif()
-
-    list (APPEND occflags ${add_hw_release_target_SOURCES_LIST})
+    list (APPEND occflags ${add_aoc_target_SOURCES_LIST})
     
     add_custom_target(${target_name_local}
         COMMAND aoc ${occflags}
