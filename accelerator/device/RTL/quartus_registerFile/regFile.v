396d99be4159f2ebb9f2c98610d958b02b7067b5
James Liu <james2006roger@ayoo.ca>
2019-12-12 10:36:09 -0500

    Added parametrizable register file RTL implementation

---
+++ accelerator/device/RTL/quartus_registerFile/regFile.v
@@ -0,0 +1,95 @@
+`timescale 1ns/1ps
+
+module regFile 
+	# (
+		parameter PORT_WIDTH = 16,
+		parameter ADDR_WIDTH = 4
+	)
+	(
+	//Write side 
+	input writeBank,
+	input writeEnable,
+	input [ADDR_WIDTH-2:0] writeAddrTransferBlock,
+	//input [ADDR_WIDTH-1:0] writeAddr1,
+	input [PORT_WIDTH-1:0] writeData0,
+	input [PORT_WIDTH-1:0] writeData1,
+
+	//Read side,
+	input readBank,
+	input [ADDR_WIDTH-1:0] readAddr0,
+	input [ADDR_WIDTH-1:0] readAddr1,
+	output reg [PORT_WIDTH-1:0] readData0,
+	output reg [PORT_WIDTH-1:0] readData1,
+
+	//Interface
+	input clock,
+	input resetn
+	);
+
+	reg [2**(ADDR_WIDTH+1)*PORT_WIDTH-1:0] registerFile;
+	//wire [2**(ADDR_WIDTH+1)-1:0] writeEnableRegisterFile0;
+	//wire [2**(ADDR_WIDTH+1)-1:0] writeEnableRegisterFile1;
+	wire [2**(ADDR_WIDTH)-1:0] writeEnableRegisterFileTransferBlock;
+
+	reg regReadBank;
+	reg [ADDR_WIDTH-1:0] regReadAddress0;
+	reg [ADDR_WIDTH-1:0] regReadAddress1;
+
+	//See intel.com/content/dam/www/programmable/us/en/pdfs/literature/catalogs/lpm.pdf
+	// lpm_decode	#(.lpm_decodes(2**(ADDR_WIDTH+1)), .lpm_type("LPM_DECODE"), .lpm_width(ADDR_WIDTH+1))
+	// write_decoder_bank0 (
+	// 	.data ({writeBank, writeAddr0}),
+	// 	.eq (writeEnableRegisterFile0)
+	// 	// synopsys translate_on
+	// 	);
+
+	// lpm_decode	#(.lpm_decodes(2**(ADDR_WIDTH+1)), .lpm_type("LPM_DECODE"), .lpm_width(ADDR_WIDTH+1))
+	// write_decoder_bank1 (
+	// 	.data ({writeBank, writeAddr1}),
+	// 	.eq (writeEnableRegisterFile1)
+	// 	// synopsys translate_on
+	// 	);
+	lpm_decode	#(.lpm_decodes(2**(ADDR_WIDTH)), .lpm_type("LPM_DECODE"), .lpm_width(ADDR_WIDTH))
+	write_decoder_wide (
+		.data ({writeBank, writeAddrTransferBlock}),
+		.eq (writeEnableRegisterFileTransferBlock)
+		// synopsys translate_on
+		);
+
+
+	//Generate the write side
+	integer regI;
+	always @ (posedge clock) begin
+		if (resetn == 1'b0) begin
+			registerFile <= {(2**(ADDR_WIDTH+1)*PORT_WIDTH){1'b0}};
+		end
+		else begin
+			for (regI=0; regI<2**(ADDR_WIDTH); regI=regI+1) begin
+				if (writeEnable == 1'b1) begin
+					if (writeEnableRegisterFileTransferBlock[regI] == 1'b1) begin
+						registerFile[(2*regI+2)*PORT_WIDTH-1 -: 2*PORT_WIDTH] <= {writeData1, writeData0};
+					end
+				end
+			end
+		end
+	end
+
+	//Generate the delay registers
+	always @ (posedge clock) begin
+		if (resetn == 1'b0) begin
+			regReadBank <= 1'b0;
+			{regReadAddress1, regReadAddress0} <= {(2*ADDR_WIDTH){1'b0}};
+		end
+		else begin
+			{regReadBank, regReadAddress1, regReadAddress0} <= {readBank, readAddr1, readAddr0};
+		end
+	end
+
+	//Generate the output select
+	always @ (*) begin
+		readData0 = registerFile[({regReadBank, regReadAddress0}+1)*PORT_WIDTH-1 -: PORT_WIDTH];
+		readData1 = registerFile[({regReadBank, regReadAddress1}+1)*PORT_WIDTH-1 -: PORT_WIDTH];
+	end
+
+
+endmodule
---
+++ accelerator/device/RTL/quartus_registerFile/top.v
@@ -0,0 +1,267 @@
+// ============================================================================
+// Copyright (c) 2013 by Terasic Technologies Inc.
+// ============================================================================
+//
+// Permission:
+//
+//   Terasic grants permission to use and modify this code for use
+//   in synthesis for all Terasic Development Boards and Altera Development 
+//   Kits made by Terasic.  Other use of this code, including the selling 
+//   ,duplication, or modification of any portion is strictly prohibited.
+//
+// Disclaimer:
+//
+//   This VHDL/Verilog or C/C++ source code is intended as a design reference
+//   which illustrates how these types of functions can be implemented.
+//   It is the user's responsibility to verify their design for
+//   consistency and functionality through the use of formal
+//   verification methods.  Terasic provides no warranty regarding the use 
+//   or functionality of this code.
+//
+// ============================================================================
+//           
+//  Terasic Technologies Inc
+//  9F., No.176, Sec.2, Gongdao 5th Rd, East Dist, Hsinchu City, 30070. Taiwan
+//  
+//  
+//                     web: http://www.terasic.com/  
+//                     email: support@terasic.com
+//
+// ============================================================================
+//Date:  Thu Jul 11 11:26:45 2013
+// ============================================================================
+
+//`define ENABLE_HPS
+
+module top(
+
+      ///////// ADC /////////
+      output             ADC_CONVST,
+      output             ADC_DIN,
+      input              ADC_DOUT,
+      output             ADC_SCLK,
+
+      ///////// AUD /////////
+      input              AUD_ADCDAT,
+      inout              AUD_ADCLRCK,
+      inout              AUD_BCLK,
+      output             AUD_DACDAT,
+      inout              AUD_DACLRCK,
+      output             AUD_XCK,
+
+      ///////// CLOCK2 /////////
+      input              CLOCK2_50,
+
+      ///////// CLOCK3 /////////
+      input              CLOCK3_50,
+
+      ///////// CLOCK4 /////////
+      input              CLOCK4_50,
+
+      ///////// CLOCK /////////
+      input              CLOCK_50,
+
+      ///////// DRAM /////////
+      output      [12:0] DRAM_ADDR,
+      output      [1:0]  DRAM_BA,
+      output             DRAM_CAS_N,
+      output             DRAM_CKE,
+      output             DRAM_CLK,
+      output             DRAM_CS_N,
+      inout       [15:0] DRAM_DQ,
+      output             DRAM_LDQM,
+      output             DRAM_RAS_N,
+      output             DRAM_UDQM,
+      output             DRAM_WE_N,
+
+      ///////// FAN /////////
+      output             FAN_CTRL,
+
+      ///////// FPGA /////////
+      output             FPGA_I2C_SCLK,
+      inout              FPGA_I2C_SDAT,
+
+      ///////// GPIO /////////
+      inout     [35:0]         GPIO_0,
+      inout     [35:0]         GPIO_1,
+ 
+
+      ///////// HEX0 /////////
+      output      [6:0]  HEX0,
+
+      ///////// HEX1 /////////
+      output      [6:0]  HEX1,
+
+      ///////// HEX2 /////////
+      output      [6:0]  HEX2,
+
+      ///////// HEX3 /////////
+      output      [6:0]  HEX3,
+
+      ///////// HEX4 /////////
+      output      [6:0]  HEX4,
+
+      ///////// HEX5 /////////
+      output      [6:0]  HEX5,
+
+`ifdef ENABLE_HPS
+      ///////// HPS /////////
+      inout              HPS_CONV_USB_N,
+      output      [14:0] HPS_DDR3_ADDR,
+      output      [2:0]  HPS_DDR3_BA,
+      output             HPS_DDR3_CAS_N,
+      output             HPS_DDR3_CKE,
+      output             HPS_DDR3_CK_N,
+      output             HPS_DDR3_CK_P,
+      output             HPS_DDR3_CS_N,
+      output      [3:0]  HPS_DDR3_DM,
+      inout       [31:0] HPS_DDR3_DQ,
+      inout       [3:0]  HPS_DDR3_DQS_N,
+      inout       [3:0]  HPS_DDR3_DQS_P,
+      output             HPS_DDR3_ODT,
+      output             HPS_DDR3_RAS_N,
+      output             HPS_DDR3_RESET_N,
+      input              HPS_DDR3_RZQ,
+      output             HPS_DDR3_WE_N,
+      output             HPS_ENET_GTX_CLK,
+      inout              HPS_ENET_INT_N,
+      output             HPS_ENET_MDC,
+      inout              HPS_ENET_MDIO,
+      input              HPS_ENET_RX_CLK,
+      input       [3:0]  HPS_ENET_RX_DATA,
+      input              HPS_ENET_RX_DV,
+      output      [3:0]  HPS_ENET_TX_DATA,
+      output             HPS_ENET_TX_EN,
+      inout       [3:0]  HPS_FLASH_DATA,
+      output             HPS_FLASH_DCLK,
+      output             HPS_FLASH_NCSO,
+      inout              HPS_GSENSOR_INT,
+      inout              HPS_I2C1_SCLK,
+      inout              HPS_I2C1_SDAT,
+      inout              HPS_I2C2_SCLK,
+      inout              HPS_I2C2_SDAT,
+      inout              HPS_I2C_CONTROL,
+      inout              HPS_KEY,
+      inout              HPS_LED,
+      inout              HPS_LTC_GPIO,
+      output             HPS_SD_CLK,
+      inout              HPS_SD_CMD,
+      inout       [3:0]  HPS_SD_DATA,
+      output             HPS_SPIM_CLK,
+      input              HPS_SPIM_MISO,
+      output             HPS_SPIM_MOSI,
+      inout              HPS_SPIM_SS,
+      input              HPS_UART_RX,
+      output             HPS_UART_TX,
+      input              HPS_USB_CLKOUT,
+      inout       [7:0]  HPS_USB_DATA,
+      input              HPS_USB_DIR,
+      input              HPS_USB_NXT,
+      output             HPS_USB_STP,
+`endif /*ENABLE_HPS*/
+
+      ///////// IRDA /////////
+      input              IRDA_RXD,
+      output             IRDA_TXD,
+
+      ///////// KEY /////////
+      input       [3:0]  KEY,
+
+      ///////// LEDR /////////
+      output      [9:0]  LEDR,
+
+      ///////// PS2 /////////
+      inout              PS2_CLK,
+      inout              PS2_CLK2,
+      inout              PS2_DAT,
+      inout              PS2_DAT2,
+
+      ///////// SW /////////
+      input       [9:0]  SW,
+
+      ///////// TD /////////
+      input              TD_CLK27,
+      input      [7:0]  TD_DATA,
+      input             TD_HS,
+      output             TD_RESET_N,
+      input             TD_VS,
+
+      ///////// VGA /////////
+      output      [7:0]  VGA_B,
+      output             VGA_BLANK_N,
+      output             VGA_CLK,
+      output      [7:0]  VGA_G,
+      output             VGA_HS,
+      output      [7:0]  VGA_R,
+      output             VGA_SYNC_N,
+      output             VGA_VS
+);
+
+
+//=======================================================
+//  REG/WIRE declarations
+//=======================================================
+localparam PORT_WIDTH = 16;
+localparam ADDR_WIDTH = 4;
+
+reg [PORT_WIDTH-1:0] counter;
+reg increment, increment_delay;
+reg update;
+wire [PORT_WIDTH-1:0] readData0;
+wire [PORT_WIDTH-1:0] readData1;
+
+//=======================================================
+//  Structural coding
+//=======================================================
+always @ (posedge CLOCK_50) begin
+      if (KEY[0] == 1'b0) begin
+            counter <= {PORT_WIDTH{1'b0}};
+            increment <= 1'b0;
+            update <= 1'b0;
+            increment_delay <= 1'b0;
+      end
+      else begin
+            increment <= 1'b0;
+            increment_delay <= increment;
+            if (increment == 1'b1) begin
+                  counter <= counter + {{(PORT_WIDTH-2){1'b0}}, 2'b11};
+            end
+
+            if (update == 1'b0) begin
+                  if (KEY[1] == 1'b0) begin
+                        increment <= 1'b1;
+                        update <= 1'b1;
+                  end
+            end 
+            else begin
+                  if (KEY[1] == 1'b1) begin
+                        update <= 1'b0;
+                  end
+            end
+      end
+end
+
+regFile #(.PORT_WIDTH(PORT_WIDTH), .ADDR_WIDTH(ADDR_WIDTH)) inst_regFile 
+      (
+            .writeBank(SW[9]),
+            .writeEnable(increment_delay),
+            //.writeAddr0(counter[ADDR_WIDTH-1:0]),
+            //.writeAddr1(counter[ADDR_WIDTH-1:0] + 1),
+            .writeAddrTransferBlock(counter[ADDR_WIDTH-1:1]),
+            .writeData0(counter),
+            .writeData1(counter+1),
+
+            .readBank(SW[9]),
+            .readAddr0(counter[ADDR_WIDTH-1:0]),
+            .readAddr1(counter[ADDR_WIDTH-1:0] + 1),
+            .readData0(readData0),
+            .readData1(readData1),
+            .clock(CLOCK_50),
+            .resetn     (KEY[0])
+      );
+
+assign LEDR[9] = ~{readData1, readData0};
+assign LEDR[7:0] = readData0[7:0];
+
+
+endmodule
