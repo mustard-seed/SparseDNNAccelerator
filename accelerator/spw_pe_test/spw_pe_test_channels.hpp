656b2ca7c42a4721f8ba3af6a1994cf14b86ef61
James Liu <james2006roger@ayoo.ca>
2021-02-08 12:47:43 -0500

    Updated the channels declaration used for the SpW PE test.

--- accelerator/spw_pe_test/spw_pe_test_channels.hpp
+++ accelerator/spw_pe_test/spw_pe_test_channels.hpp
@@ -6,9 +6,10 @@
 #pragma OPENCL EXTENSION cl_intel_channels : enable
 
 //Sets the channels' depths more than 1 to avoid potential deadlock
-channel t_pe_w_block channel_weight[PE_ROWS_PER_GROUP][2] __attribute__((depth(2))); 
+channel t_pe_w_block channel_weight[PE_ROWS_PER_GROUP][2] __attribute__((depth(1))); 
+channel t_pe_w_block channel_weight_local[PE_ROWS][1] __attribute__((depth(1)));
 
-channel t_pe_a_block channel_activation[PE_ROW_GROUPS+1][1] __attribute__((depth(2))); 
+channel t_pe_a_block channel_activation[PE_ROW_GROUPS+1][1] __attribute__((depth(1))); 
 
-channel t_conv_drain_multiple_tagged channel_drain_conv_local[1][1] __attribute__((depth(2)));
+channel t_conv_drain_multiple_tagged channel_drain_conv_local[1][1] __attribute__((depth(1)));
 #endif
